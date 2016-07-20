cef0e1d38858cfeb200578c2b434c9f384f5bd0b
ABHINAV THOMAS <abhinav@localhost.localdomain>
2016-07-20 19:18:29 +0530

    without audio

--- video_downloader.py
+++ video_downloader.py
@@ -1,122 +1,196 @@
 #!/usr/bin/env/python
 """
-	Script to download from youtube playlist.
-	Author : Abhinav Thomas
-	
+    Script to download from youtube playlist.
+    Author : Abhinav Thomas
+    
 """
 import pickle
 import logging
 import time
+import os
 
 def urlfetch():
-	try:
-		fp= open('config.txt','r')
-		logging.info("config file opened")
-		url=fp.read()
-		fp.close()
-		logging.info("url read. returning url...")
-		return url
-	except IOError:
-		logging.info("config file not found")
-		fp = open('config.txt','w')
-		logging.info("config created")
-		url = raw_input("Enter the url : ")
-		fp.write(url)
-		logging.info("config file opened and new url is inputed")
-		fp.close()
-		return url
-		
-def download_stack_reader():
-	try:
-		fp=open("downloaded_files.pkl","rb")
-	except IOError:
-		fp=open("downloaded_files.pkl","wb")
-		fp.close()
-		fp=open("downloaded_files.pkl","rb")		
-		
-	logging.info("downloaded_files opened .... ")
-	l=[]
-	while(True):
-		try:
-			l.append(pickle.load(fp))
-			logging.info("loaded stack of downloaded files..")
-		except EOFError:
-			break
-	fp.close()		
-	return l
+    try:
+        fp= open('config.txt','r')
+        logging.info("config file opened")
+        url=fp.read().splitlines()
+        fp.close()
+        logging.info("url read. returning url...")
+        return url
+    except IOError:
+        logging.info("config file not found")
+        fp = open('config.txt','w')
+        logging.info("config created")
+        vurl = raw_input("Enter the video url : ")
+        fp.write(vurl)
+        fp.write('\n')
+        aurl = raw_input("Enter the audio url : ")
+        fp.write(aurl)
+        logging.info("config file opened and new url is inputed")
+        fp.close()
+        return [vurl,aurl]
+        
+def v_download_stack_reader():
+    try:
+        fp=open("v_downloaded_files.pkl","rb")
+    except IOError:
+        fp=open("v_downloaded_files.pkl","wb")
+        fp.close()
+        fp=open("v_downloaded_files.pkl","rb")        
+        
+    logging.info("downloaded_files opened .... ")
+    l=[]
+    while(True):
+        try:
+            l.append(pickle.load(fp))
+            logging.info("loaded stack of downloaded files..")
+        except EOFError:
+            break
+    fp.close()      
+    return l
 
-def download_stack_updater(downloaded):
-	fp=open("downloaded_files.pkl","ab")
-	pickle.dump(downloaded,fp)
-	fp.close()
-	
-def display(v): 
-	print "Title	: ",v.title
-	print "Duration : ",v.duration
-	print "Rating	: ",v.rating
-	print "Author	: ",v.author
-	print "Views	: ",v.viewcount
-	for s in v.videostreams:
-	    print(s.mediatype, s.extension, s.quality)
-	print "=========Downloading best quality video==========="
-	logging.info("=========Downloading best quality video===========")
+def a_download_stack_reader():
+    try:
+        fp=open("a_downloaded_files.pkl","rb")
+    except IOError:
+        fp=open("a_downloaded_files.pkl","wb")
+        fp.close()
+        fp=open("a_downloaded_files.pkl","rb")        
+        
+    logging.info("downloaded_files opened .... ")
+    l=[]
+
+    while(True):
+        try:
+            l.append(pickle.load(fp))
+            logging.info("loaded stack of downloaded files..")
+        except EOFError:
+            break
+    fp.close()      
+    return l
+
+def v_download_stack_updater(downloaded):
+    fp=open("v_downloaded_files.pkl","ab")
+    pickle.dump(downloaded,fp)
+    fp.close()
+    
+def a_download_stack_updater(downloaded):
+    fp=open("a_downloaded_files.pkl","ab")
+    pickle.dump(downloaded,fp)
+    fp.close()
+    
+def display(v,a): 
+    print "Title    : ",v.title
+    print "Duration : ",v.duration
+    print "Rating   : ",v.rating
+    print "Author   : ",v.author
+    print "Views    : ",v.viewcount
+    
+    if a=='v':
+        for s in v.videostreams:
+            print(s.mediatype, s.extension, s.quality)
+        print "=========Downloading best quality video==========="
+        logging.info("=========Downloading best quality video===========")
+    else:
+
+        for a in v.audiostreams:
+            print(a.bitrate, a.extension, a.get_filesize())
+        print "=========Downloading best quality audio==========="
+        logging.info("=========Downloading best quality audio===========")
+
 
 def downloader():
-	while(True):
+    while(True):
 
-		try:
-			import pafy
-			break
+        try:
+            import pafy
+            break
 
-		except ImportError:
-			print "Inside exception ImportError"
-			from os import system
+        except ImportError:
+            print "Inside exception ImportError"
+            from os import system
 
-			try:
-				print "trying pafy installation"
-				system("sudo pip install pafy")
-				logging.info("pafy has installed...")
-				print "pip installation finished"
-			except:
-				print "Couldn't install pip. Inside pip exception"
-				logging.error("couldn't install pafy")
-				system("wget https://bootstrap.pypa.io/get-pip.py")
-				logging.info("trying to install pip..")
-				system("sudo python get-pip.py")
-				logging.info("Successfully installed pip...")
-				print "pip already install"
+            try:
+                print "trying pafy installation"
+                system("sudo pip install pafy")
+                logging.info("pafy has installed...")
+                print "pip installation finished"
+            except:
+                print "Couldn't install pip. Inside pip exception"
+                logging.error("couldn't install pafy")
+                system("wget https://bootstrap.pypa.io/get-pip.py")
+                logging.info("trying to install pip..")
+                system("sudo python get-pip.py")
+                logging.info("Successfully installed pip...")
+                print "pip already install"
 
-	to_download=0 
-	downloaded = download_stack_reader()
-	url = urlfetch()
-	playlist = pafy.get_playlist(url)
-	logging.info("playlist created")
-    
-	while(True):
-		try:
-			video = playlist['items'][to_download]['pafy']
-			logging.info(playlist['items'][to_download]['pafy'])
-			if video.videoid not in downloaded :
-				display(video)
-				download_url = video.getbest()
-				logging.info("Video found")
-				filename = download_url.download()
-				logging.info("Downloading...")
-				download_stack_updater(video.videoid)
-			else:
-				print "Video already downloaded"
-			logging.info("stack updated with new value")
-			to_download+=1
+    v_to_download=0 
+    a_to_download=0 
+    vdownloaded = v_download_stack_reader()
+    adownloaded = a_download_stack_reader()
+    url = urlfetch()
+    vplaylist = pafy.get_playlist(url[0])
+    aplaylist = pafy.get_playlist(url[1])
+    logging.info("playlist created")
+    a,v=0,0
+    while(True):
+        a,v=0,0    
+        try:
+            video = vplaylist['items'][v_to_download]['pafy']
+            logging.info(vplaylist['items'][v_to_download]['pafy'])
+            if video.videoid not in vdownloaded :
+                display(video,'v')
+                download_url = video.getbest()
+                logging.info("Video found")
+                os.chdir('./video')
+                filename = download_url.download()
+                os.chdir('../')
+                logging.info("Downloading...")
+                v_download_stack_updater(video.videoid)
+            else:
+                print "Video already downloaded"
+            logging.info("stack updated with new value")
+            
+            v_to_download+=1
 
-		except IndexError:
-			print "Downloading finished"
-			logging.info("download finished")
-			break
+        except IndexError:
+
+            print "Video Downloading finished"
+            v=1
+            logging.info("download finished")
+
+        try:
+            video = aplaylist['items'][a_to_download]['pafy']
+            logging.info(aplaylist['items'][a_to_download]['pafy'])
+            if video.videoid not in adownloaded :
+                display(video,'a')
+                download_url = video.getbestaudio()
+                logging.info("Audio found")
+                os.chdir('./audio')
+                filename = download_url.download()
+                os.chdir('../')
+                logging.info("Downloading...")
+                a_download_stack_updater(video.videoid)
+            else:
+                print "Audio already downloaded"
+            print("outside audio else")
+            logging.info("stack updated with new value")
+            
+            a_to_download+=1
+
+        except IndexError:
+
+            print "Audio Downloading finished"
+            a=1
+            logging.info("download finished")
+
+        if(a==1 and v==1):
+            break
 
 if __name__=='__main__':
-	logging.basicConfig(filename="downloader_log.log", level=logging.DEBUG)
-	print "GPL... (c) Abhinav Thomas .."
-	print "All rights reserved.."
-	while True:
-	        downloader()
-        	time.sleep(3600) # 3600 seconds = 1 hour
+    logging.basicConfig(filename="downloader_log.log", level=logging.DEBUG)
+    print "GPL .v2... (c) Abhinav Thomas .."
+    print "All rights reserved.."
+    while True:
+            downloader()
+            time.sleep(20) # 60 seconds = 1 minute
